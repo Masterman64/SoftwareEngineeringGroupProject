a4b078df9ce0ea147eb27b4db75b3bb737a2b690
nikolassparrow <nlm024@shsu.edu>
2022-10-21 02:37:27 -0500

    added sign in page, the home page, firebase, starter UI

---
+++ pantry_organizer/lib/homeLanding.dart
@@ -0,0 +1,37 @@
+// ignore_for_file: prefer_const_constructors, prefer_interpolation_to_compose_strings, avoid_print, prefer_const_literals_to_create_immutables, library_private_types_in_public_api
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class HomeLanding extends StatefulWidget {
+  const HomeLanding({Key? key}) : super(key: key);
+
+  @override
+  State<HomeLanding> createState() => _HomeLandingState();
+}
+
+class _HomeLandingState extends State<HomeLanding> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('testing home page'),
+            MaterialButton(
+              onPressed: () {
+                FirebaseAuth.instance.signOut();
+              },
+              color: Colors.deepOrange[300],
+              child: 
+              Text('sign out'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- pantry_organizer/lib/main.dart
+++ pantry_organizer/lib/main.dart
@@ -1,20 +1,37 @@
 // ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
+import 'loginLanding.dart';
+import 'homeLanding.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(MainApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: "AIzaSyBxFUAqvcEzQNovauSwd8Eabtc1m3tUljo",
+      authDomain: "seproject-group10-f7a99.firebaseapp.com",
+      projectId: "seproject-group10-f7a99",
+      storageBucket: "seproject-group10-f7a99.appspot.com",
+      messagingSenderId: "303276218464",
+      appId: "1:303276218464:web:a483f2c98dee8a10e9cb05",
+      measurementId: "G-KRWRVP2ZG9"
+    )
+  );
+  
+  runApp(const MainApp());
+}
 
-class MainApp extends StatelessWidget{
+class MainApp extends StatelessWidget {
+  const MainApp({Key? key}) : super(key: key);
+  
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark().copyWith(
-        colorScheme: ColorScheme.light().copyWith(primary: Colors.purple), // easily the best line of code out of this whole damn thing 😎
-      ), 
-      home: Scaffold(
-        appBar: AppBar(title: Text('Pantry Organizer')),
-        body: Center(
-          child: Text('Testing for now')
-        )
-      ));
+        theme: ThemeData.dark().copyWith(
+          colorScheme: ColorScheme.light().copyWith(
+            primary: Colors.purple), // easily the best line of code out of this whole damn thing 😎
+        ),
+        home: HomeLanding(),
+    );
   }
 }
