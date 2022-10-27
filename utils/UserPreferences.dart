9cf2a77855a4fd792a4287e2fcf766507fce30a4
LissandraFanini <112572651+LissandraFanini@users.noreply.github.com>
2022-10-27 15:31:17 -0500

    Add files via upload

---
+++ utils/UserPreferences.dart
@@ -0,0 +1,11 @@
+import 'package:user_profile_example/model/user.dart';
+
+class UserPreferences {
+  static const myUser = User(
+    imagePath:
+    name: 'Jacob >:(',
+    email: '',
+    about:
+    isDarkMode: ,
+  );
+}
