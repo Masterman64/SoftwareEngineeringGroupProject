020ee164c23951843cd58c42065e3a393e23027d
Jacob Jones <jonesjd2064@gmail.com>
2022-11-08 20:05:38 -0600

    Implemented the rest of Lissandra's files

---
+++ pantry_organizer/lib/pages/profile.dart
@@ -0,0 +1,38 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:convex_bottom_bar/convex_bottom_bar.dart';
+
+class ProfilePage extends StatelessWidget {
+  static const String page3 = '/profile';
+  const ProfilePage({super.key});
+  final double coverHeight = 280;
+  final double profileHeight = 140;
+  final double top = 210;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+          clipBehavior: Clip.none,
+          alignment: Alignment.center,
+          children: [
+            buildCoverImage(),
+            Positioned(top: top, child: buildProfileImage()),
+          ]),
+    );
+  }
+
+  Widget buildCoverImage() => Container(
+        color: Colors.deepPurple,
+        child: Image.network(
+          'https://static8.depositphotos.com/1392258/865/i/450/depositphotos_8656042-stock-photo-upscale-kitchen-with-breakfast-bar.jpg',
+          width: double.infinity,
+          height: coverHeight,
+          fit: BoxFit.cover,
+        ),
+      );
+  Widget buildProfileImage() => CircleAvatar(
+        radius: profileHeight / 2,
+        backgroundColor: Colors.white,
+        backgroundImage: const NetworkImage('https://i.imgur.com/62B53PO.png'),
+      );
+}
